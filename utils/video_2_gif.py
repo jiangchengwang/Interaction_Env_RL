2a5cf2138e0af7892aefa99ae4ed6a48e54c807e
王江成 <1239974410@qq.com>
2025-02-14 10:53:28 +0800

    add environment.yaml

--- utils/video_2_gif.py
+++ utils/video_2_gif.py
@@ -11,9 +11,9 @@ def save_video_to_gif(video_path, output_path):
 
     # 读取视频
     clip = VideoFileClip(video_path)
-    end = clip.end if clip.duration < 10 else clip.start + 10
-    # 可以选择截取视频的一部分，比如从10秒到20秒
-    clip = clip.subclip(clip.start, end)  # 可选的，调整时间范围
+    # end = clip.end if clip.duration < 10 else clip.start + 10
+    # # 可以选择截取视频的一部分，比如从10秒到20秒
+    # clip = clip.subclip(clip.start, end)  # 可选的，调整时间范围
     # 将视频转换为GIF
     clip.write_gif(output_path, fps=10)  # fps控制GIF的帧率，调整这个值可以改变GIF的流畅度
     clip.close()
